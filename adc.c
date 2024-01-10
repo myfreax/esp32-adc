5016007e984b1ceed4bb1e4bb241e8b4dc35bdb6
myfreax <web@myfreax.com>
2024-01-10 19:12:07 +0800

    feat: support great than 5.0 version of ESP-IDF

--- adc.c
+++ adc.c
@@ -1,5 +1,97 @@
 #include "adc.h"
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#include "esp_log.h"
+static const char* TAG = "ADC";
 
+static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
+                                 adc_atten_t atten,
+                                 adc_cali_handle_t* out_handle);
+
+adc_t* adc_config(adc_channel_t channel, adc_bitwidth_t width_bit,
+                  adc_atten_t atten) {
+  adc_t* adc = malloc(sizeof(adc_t));
+  adc->atten = atten;
+  adc->channel = channel;
+  adc->adc_oneshot_handle = malloc(sizeof(adc_oneshot_unit_handle_t));
+  adc->adc_cali_handle = malloc(sizeof(adc_cali_handle_t));
+  adc_oneshot_unit_init_cfg_t unit_config = {
+      .unit_id = ADC_UNIT_1,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&unit_config, &adc->adc_oneshot_handle));
+  adc_oneshot_chan_cfg_t chan_config = {
+      .bitwidth = width_bit,
+      .atten = atten,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc->adc_oneshot_handle, channel,
+                                             &chan_config));
+  bool calibration =
+      adc_calibration_init(ADC_UNIT_1, channel, atten, &adc->adc_cali_handle);
+  adc->calibration = calibration;
+  return adc;
+}
+
+int adc_voltage(adc_t* adc) {
+  int adc_raw;
+  int voltage;
+  adc_oneshot_read(adc->adc_oneshot_handle, adc->channel, &adc_raw);
+  if (adc->calibration) {
+    adc_cali_raw_to_voltage(adc->adc_cali_handle, adc_raw, &voltage);
+    return voltage;
+  }
+  return adc_raw;
+}
+
+static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
+                                 adc_atten_t atten,
+                                 adc_cali_handle_t* out_handle) {
+  adc_cali_handle_t handle = NULL;
+  esp_err_t ret = ESP_FAIL;
+  bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+  if (!calibrated) {
+    ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+    adc_cali_curve_fitting_config_t cali_config = {
+        .unit_id = unit,
+        .chan = channel,
+        .atten = atten,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+    if (ret == ESP_OK) {
+      calibrated = true;
+    }
+  }
+#endif
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+  if (!calibrated) {
+    ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+    adc_cali_line_fitting_config_t cali_config = {
+        .unit_id = unit,
+        .atten = atten,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+    if (ret == ESP_OK) {
+      calibrated = true;
+    }
+  }
+#endif
+
+  *out_handle = handle;
+  if (ret == ESP_OK) {
+    ESP_LOGI(TAG, "Calibration Success");
+  } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+    ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+  } else {
+    ESP_LOGE(TAG, "Invalid arg or no memory");
+  }
+
+  return calibrated;
+}
+
+#else
 esp_adc_cal_characteristics_t* adc_config(adc1_channel_t adc_channel,
                                           adc_bits_width_t width_bit,
                                           uint32_t default_vref,
@@ -24,3 +116,4 @@ uint32_t adc_voltage(adc1_channel_t adc_channel,
   uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
   return voltage;
 }
+#endif
--- include/adc.h
+++ include/adc.h
@@ -1,10 +1,31 @@
 #pragma once
+#include "esp_idf_version.h"
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#include <stdbool.h>
+
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "esp_adc/adc_oneshot.h"
+
+typedef struct {
+  adc_channel_t channel;
+  adc_atten_t atten;
+  adc_oneshot_unit_handle_t adc_oneshot_handle;
+  bool calibration;
+  adc_cali_handle_t adc_cali_handle;
+} adc_t;
+
+adc_t* adc_config(adc_channel_t channel, adc_bitwidth_t width_bit,
+                  adc_atten_t atten);
+int adc_voltage(adc_t* adc);
+#else
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
-
 esp_adc_cal_characteristics_t* adc_config(adc1_channel_t adc_channel,
                                           adc_bits_width_t width_bit,
                                           uint32_t default_vref,
                                           adc_atten_t atten);
 uint32_t adc_voltage(adc1_channel_t adc_channel,
                      esp_adc_cal_characteristics_t* adc_chars);
+#endif
